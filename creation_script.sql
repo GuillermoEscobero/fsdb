4a233ef9efc3ce6052f35515bf88dd650e5cda38
GuillermoEscobero <100346060@alumnos.uc3m.es>
2017-03-05 23:37:01 +0100

    Añadidos nuevos checks y script para importar algunas tablas

--- creation_script.sql
+++ creation_script.sql
@@ -10,15 +10,13 @@ CREATE TABLE Clients
     sec_surname  VARCHAR2 (100 CHAR) ,
     birthdate    DATE NOT NULL ,
     phonen       NUMBER(14) ,
-    zipcode      VARCHAR2 (10 CHAR)  NOT NULL ,
-    town         VARCHAR2 (100 CHAR) NOT NULL ,
-    address      VARCHAR2 (150 CHAR) NOT NULL ,
-    country      VARCHAR2 (100 CHAR) NOT NULL ,
     CONSTRAINT PK_clients PRIMARY KEY (clientid) ,
     CONSTRAINT U_clients1 UNIQUE (email) ,
     CONSTRAINT U_clients2 UNIQUE (dni) ,
     CONSTRAINT U_clients3 UNIQUE (phonen),
-    CONSTRAINT CH_clients CHECK (phonen > 0)
+    CONSTRAINT CK_clients1 CHECK (REGEXP_LIKE(dni, '^[0-9]{8}+[A-Za-z]{1}$')) ,
+    CONSTRAINT CK_clients2 CHECK (REGEXP_LIKE(email, '^[A-Za-z]+[A-Za-z0-9.]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$')) ,
+    CONSTRAINT CK_clients3 CHECK (phonen > 0)
 	) ;
 
 CREATE TABLE Contracts_types
@@ -40,7 +38,11 @@ CREATE TABLE Contracts_types
 CREATE TABLE Contracts
   (
     contractid    VARCHAR2 (10 CHAR) ,
-    clientid      VARCHAR(15 CHAR) NOT NULL , -- Esto con el trigger al insertar, porque luego se puede borrar si lo piden
+    clientid      VARCHAR2 (15 CHAR) NOT NULL , -- Esto con el trigger al insertar, porque luego se puede borrar si lo piden
+    zipcode       VARCHAR2 (10 CHAR)  NOT NULL ,
+    town          VARCHAR2 (100 CHAR) NOT NULL ,
+    address       VARCHAR2 (150 CHAR) NOT NULL ,
+    country       VARCHAR2 (100 CHAR) NOT NULL ,
     startdate     DATE NOT NULL ,
     enddate       DATE ,
     contract_type VARCHAR2 (50 CHAR) NOT NULL ,
@@ -86,7 +88,7 @@ CREATE TABLE Movies
    CONSTRAINT CK_movies3 CHECK (NUM_CRITIC_FOR_REVIEWS >= 0) ,
    CONSTRAINT CK_movies4 CHECK (GROSS >= 0) ,
    CONSTRAINT CK_movies5 CHECK (BUDGET >= 0) ,
-   CONSTRAINT CK_movies6 CHECK (TITLE_YEAR >= TO_DATE('1895-12-28', 'YYYY-MM-DD')) ,
+   CONSTRAINT CK_movies6 CHECK (TITLE_YEAR >= TO_DATE('1895-12-28', 'YYYY-MM-DD')) , -- First movie of history
    CONSTRAINT CK_movies7 CHECK (IMDB_SCORE BETWEEN 0.0 AND 10.0 ) ,
    CONSTRAINT CK_movies8 CHECK (ASPECT_RATIO > 0) ,
    CONSTRAINT CK_movies9 CHECK (CONTENT_RATING IN ('TV-14', 'R', 'Not Rated', 'Unrated', 'NC-17', 'PG-13', 'TV-MA', 'PG', 'TV-Y7', 'M', 'GP', 'TV-Y' ,'X', 'TV-PG', 'Passed', 'G','TV-G','Approved' )),
@@ -109,11 +111,11 @@ CREATE TABLE TVSeries
    AVGDURATION   NUMBER(3) ,
    EPISODES      NUMBER(3) ,
    CONSTRAINT PK_TVseries PRIMARY KEY (TITLE) ,
-   CONSTRAINT CH_TVseries1 CHECK (TOTAL_SEASONS <= SEASON) ,
-   CONSTRAINT CH_TVseries2 CHECK (TOTAL_SEASONS >= 0) ,
-   CONSTRAINT CH_TVseries3 CHECK (SEASON >= 0) ,
-   CONSTRAINT CH_TVseries4 CHECK (AVGDURATION >= 0) ,
-   CONSTRAINT CH_TVseries5 CHECK (EPISODES >= 0)
+   CONSTRAINT CK_TVseries1 CHECK (TOTAL_SEASONS <= SEASON) ,
+   CONSTRAINT CK_TVseries2 CHECK (TOTAL_SEASONS >= 0) ,
+   CONSTRAINT CK_TVseries3 CHECK (SEASON >= 0) ,
+   CONSTRAINT CK_TVseries4 CHECK (AVGDURATION >= 0) ,
+   CONSTRAINT CK_TVseries5 CHECK (EPISODES >= 0)
   ) ;
 
 CREATE TABLE Taps
@@ -123,11 +125,14 @@ CREATE TABLE Taps
    DURATION NUMBER(15) ,
    SEASON   NUMBER(15) ,
    EPISODE  NUMBER(15) ,
-   VIEWDATE DATE,
+   VIEWDATE DATE ,
    VIEWHOUR DATE ,
    VIEWPCT  VARCHAR2(5 CHAR) ,
    CONSTRAINT PK_Taps PRIMARY KEY (CLIENT, TITLE, VIEWDATE, VIEWHOUR) ,
-   CONSTRAINT FK_Taps FOREIGN KEY (Client) REFERENCES Clients(clientid)
-   -- SI SE ELIMINA UN USAURIO HAY QUE USAR UN TRIGGER PARA QUE CLIENT PUE3DA SER NULL AL BORRARSE
+   CONSTRAINT FK_Taps FOREIGN KEY (Client) REFERENCES Clients(clientid) ,
+   CONSTRAINT CK_Taps1 CHECK (DURATION >= 0) ,
+   CONSTRAINT CK_Taps2 CHECK (SEASON >= 0) ,
+   CONSTRAINT CK_Taps3 CHECK (EPISODE >= 0) ,
+   CONSTRAINT CK_Taps4 CHECK (VIEWPCT BETWEEN 0 AND 100) --MIRAR EL TEMA DEL PORCENTAJE
    --CHECK QUE VIEWHOUR SEA UNA HORA Y UNOS MINUTOS (EL PAPER DICE QUE CON PRECISION DE MINUTOS)
    ) ;
