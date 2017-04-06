4f895753bd2ddc80db4764456506ec409657736c
GuillermoEscobero <100346060@alumnos.uc3m.es>
2017-04-06 19:23:49 +0200

    Create function.sql

---
+++ function.sql
@@ -0,0 +1,19 @@
+-- Bill a given month/year to a given customer with a given product;
+-- Returns NUMBER (6.2).
+CREATE OR REPLACE FUNCTION bill(
+  date_bill IN DATE,
+  client IN VARCHAR2(XXX),
+  product IN VARCHAR2(XXX))
+  RETURN NUMBER
+  IS total NUMBER(6,2);
+  BEGIN
+    SELECT FEE
+    INTO total
+    FROM CONTRACTS
+    JOIN CLIENTS ON CONTRACTS.CLIENTID=CLIENTS.CLIENTID
+    JOIN PRODUCTS ON CONTRACT_TYPE=TYPE
+    WHERE (client=CONTRACTS.CLIENTID)
+    AND (date_bill BETWEEN STARTDATE AND ENDDATE)
+    AND (product=CONTRACTS.CONTRACT_TYPE);
+    RETURN(total);
+  END;
