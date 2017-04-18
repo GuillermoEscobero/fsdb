4332ab53979489aec7d3db91d7dc802ac78dbbab
GuillermoEscobero <100346060@alumnos.uc3m.es>
2017-04-18 11:01:23 +0200

    Queries to lowercase

--- queries_script.sql
+++ queries_script.sql
@@ -7,24 +7,24 @@
 -- a) Alphabetical list (by surname, name) of all clients with active contract
 -- in the date when the query is run, along with start and end dates and type
 -- (PPC/PPV) of the contract.
-SELECT SURNAME, NAME, STARTDATE, ENDDATE, TYPE
-FROM CLIENTS
-NATURAL JOIN CONTRACTS
-JOIN PRODUCTS ON CONTRACT_TYPE=PRODUCT_NAME
-WHERE (SYSDATE BETWEEN STARTDATE AND ENDDATE)
-OR (ENDDATE IS NULL AND STARTDATE<=SYSDATE)
-ORDER BY SURNAME, NAME;
+SELECT surname, name, startdate, enddate, type
+FROM clients
+NATURAL JOIN contracts
+JOIN products ON contract_type=product_name
+WHERE (SYSDATE BETWEEN startdate AND enddate)
+OR (enddate IS NULL AND startdate<=SYSDATE)
+ORDER BY surname, name;
 -- 4323 rows (at 17/4/2017)
 
 
 -- b) Top 5 stars involved in more movies from USA.
 SELECT * FROM (
-SELECT ACTOR, COUNT(TITLE)
-FROM CASTS
-JOIN MOVIES ON TITLE=MOVIE_TITLE
-WHERE COUNTRY='USA'
-GROUP BY ACTOR
-ORDER BY COUNT(TITLE) DESC
+SELECT actor, COUNT(title)
+FROM casts
+JOIN movies ON title=movie_title
+WHERE country='USA'
+GROUP BY actor
+ORDER BY COUNT(title) DESC
 ) WHERE ROWNUM<=5;
 -- Robert De Niro	45
 -- Morgan Freeman	36
@@ -35,34 +35,34 @@ ORDER BY COUNT(TITLE) DESC
 
 -- c) Clients who have bought license for a complete season of any TVseries
 -- (include the title and season).
-SELECT CLIENT, TITLE, SEASON
-FROM LIC_SERIES
-NATURAL JOIN SEASONS
-GROUP BY CLIENT, TITLE, SEASON, EPISODES
-HAVING COUNT(EPISODE)=EPISODES;
+SELECT client, title, season
+FROM lic_series
+NATURAL JOIN seasons
+GROUP BY client, title, season, episodes
+HAVING COUNT(episode)=episodes;
 -- 2104 rows
 
 
 -- d) Top-Star: actor/actress who had got more taps (summing up all the taps of
 -- his/her movies) for each month (should retrieve a row for each month since
 -- the database is on duty).
-SELECT A.month, ACTOR, top
+SELECT A.month, actor, top
 FROM (
 		SELECT MAX(counter) as top, month
 		FROM (
-			SELECT TO_CHAR(VIEW_DATETIME, 'MON-YYYY') AS month, ACTOR, COUNT(TITLE) AS counter
-			FROM TAPS_MOVIES
-			NATURAL JOIN CASTS
-			GROUP BY TO_CHAR(VIEW_DATETIME, 'MON-YYYY'), ACTOR
+			SELECT TO_CHAR(view_datetime, 'MON-YYYY') AS month, actor, COUNT(title) AS counter
+			FROM taps_movies
+			NATURAL JOIN casts
+			GROUP BY TO_CHAR(view_datetime, 'MON-YYYY'), actor
 		)
 		GROUP BY month
 ) A
 JOIN
 (
-	SELECT TO_CHAR(VIEW_DATETIME, 'MON-YYYY') AS month, ACTOR, COUNT(TITLE) AS counter
-	FROM TAPS_MOVIES
-	NATURAL JOIN CASTS
-	GROUP BY TO_CHAR(VIEW_DATETIME, 'MON-YYYY'), ACTOR
+	SELECT TO_CHAR(view_datetime, 'MON-YYYY') AS month, actor, COUNT(title) AS counter
+	FROM taps_movies
+	NATURAL JOIN casts
+	GROUP BY TO_CHAR(view_datetime, 'MON-YYYY'), actor
 ) B
 ON top=counter AND A.month=B.month
 ORDER BY TO_DATE(month, 'MON-YYYY');
