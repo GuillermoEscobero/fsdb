f06da95389bad7e802f349fc6bf8e89a0e2469ba
GuillermoEscobero <100346060@alumnos.uc3m.es>
2017-04-18 12:56:03 +0200

    Add NON_VALID_TAP_SERIES

--- triggers_script.sql
+++ triggers_script.sql
@@ -6,8 +6,25 @@
 
 -- b) Prevent non-valid visualizations regarding contracts (there cannot be taps
 -- in a date out of contract).
-CREATE OR REPLACE TRIGGER NON_VALID_VISUALIZATIONS
+CREATE OR REPLACE TRIGGER NON_VALID_TAP_MOVIES
 BEFORE INSERT ON taps_movies
+FOR EACH ROW
+  DECLARE
+    contractid VARCHAR2(10 CHAR);
+    startdate DATE;
+    enddate DATE;
+  BEGIN
+    SELECT contractid, startdate, enddate INTO contractid, startdate, enddate
+    FROM contracts
+    WHERE contractid = :NEW.contractid;
+    IF NOT ((:NEW.view_datetime BETWEEN startdate AND enddate)
+      OR (enddate IS NULL AND startdate<=:NEW.view_datetime)) THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Tap date out of contract');
+    END IF;
+  END;
+
+CREATE OR REPLACE TRIGGER NON_VALID_TAP_SERIES
+BEFORE INSERT ON taps_series
 FOR EACH ROW
   DECLARE
     contractid VARCHAR2(10 CHAR);
