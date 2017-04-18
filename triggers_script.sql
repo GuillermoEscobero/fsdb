23e5f5ac657edcba46ac07ddda06e688404e5f37
GuillermoEscobero <100346060@alumnos.uc3m.es>
2017-04-18 11:10:24 +0200

    Trigger to lower case

--- triggers_script.sql
+++ triggers_script.sql
@@ -7,18 +7,18 @@
 -- b) Prevent non-valid visualizations regarding contracts (there cannot be taps
 -- in a date out of contract).
 CREATE OR REPLACE TRIGGER NON_VALID_VISUALIZATIONS
-BEFORE INSERT ON TAPS_MOVIES
+BEFORE INSERT ON taps_movies
 FOR EACH ROW
   DECLARE
     contractid VARCHAR2(10 CHAR);
     startdate DATE;
     enddate DATE;
   BEGIN
-    SELECT CONTRACTID, STARTDATE, ENDDATE INTO contractid, startdate, enddate
-    FROM CONTRACTS
-    WHERE CONTRACTID = :NEW.CONTRACTID;
-    IF NOT ((:NEW.VIEW_DATETIME BETWEEN startdate AND enddate)
-      OR (ENDDATE IS NULL AND STARTDATE<=:NEW.VIEW_DATETIME)) THEN
+    SELECT contractid, startdate, enddate INTO contractid, startdate, enddate
+    FROM contracts
+    WHERE contractid = :NEW.contractid;
+    IF NOT ((:NEW.view_datetime BETWEEN startdate AND enddate)
+      OR (enddate IS NULL AND startdate<=:NEW.view_datetime)) THEN
         RAISE_APPLICATION_ERROR(-20001, 'Tap date out of contract');
     END IF;
   END;
