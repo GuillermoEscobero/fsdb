572433b7fe23f9f48d89a288332349d783935174
Guille Escobero <GuillermoEscobero@users.noreply.github.com>
2017-05-03 14:31:22 +0200

    Create clusters.sql

---
+++ clusters.sql
@@ -0,0 +1,3 @@
+CREATE CLUSTER movie_cluster(movie_title VARCHAR2(100 CHAR));
+CREATE CLUSTER client_cluster(clientid VARCHAR2(15 CHAR));
+CREATE CLUSTER contract_cluster(contractid VARCHAR2(10 CHAR));
