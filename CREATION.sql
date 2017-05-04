5817bd3db41df58026501fe7d54d245c5c6d7acc
Raul Olmedo <100346073@alumnos.uc3m.es>
2017-05-04 12:37:38 +0200

    Mierdas puras

--- CREATION.sql
+++ CREATION.sql
@@ -15,52 +15,64 @@
 -- ----------------------------------------------------
 
 DROP TABLE MOVIES CASCADE CONSTRAINTS;
-DROP TABLE GENRES_MOVIES CASCADE CONSTRAINTS; 
-DROP TABLE keywords_movies CASCADE CONSTRAINTS; 
+DROP TABLE GENRES_MOVIES CASCADE CONSTRAINTS;
+DROP TABLE keywords_movies CASCADE CONSTRAINTS;
 DROP TABLE PLAYERS CASCADE CONSTRAINTS;
 DROP TABLE CASTS CASCADE CONSTRAINTS;
-DROP TABLE SERIES CASCADE CONSTRAINTS; 
-DROP TABLE SEASONS CASCADE CONSTRAINTS; 
-DROP TABLE CLIENTS CASCADE CONSTRAINTS; 
-DROP TABLE PRODUCTS CASCADE CONSTRAINTS; 
-DROP TABLE CONTRACTS CASCADE CONSTRAINTS; 
-DROP TABLE TAPS_MOVIES CASCADE CONSTRAINTS; 
-DROP TABLE TAPS_SERIES CASCADE CONSTRAINTS; 
-DROP TABLE LIC_MOVIES CASCADE CONSTRAINTS; 
-DROP TABLE LIC_SERIES CASCADE CONSTRAINTS; 
-DROP TABLE INVOICES CASCADE CONSTRAINTS; 
-
-
+DROP TABLE SERIES CASCADE CONSTRAINTS;
+DROP TABLE SEASONS CASCADE CONSTRAINTS;
+DROP TABLE CLIENTS CASCADE CONSTRAINTS;
+DROP TABLE PRODUCTS CASCADE CONSTRAINTS;
+DROP TABLE CONTRACTS CASCADE CONSTRAINTS;
+DROP TABLE TAPS_MOVIES CASCADE CONSTRAINTS;
+DROP TABLE TAPS_SERIES CASCADE CONSTRAINTS;
+DROP TABLE LIC_MOVIES CASCADE CONSTRAINTS;
+DROP TABLE LIC_SERIES CASCADE CONSTRAINTS;
+DROP TABLE INVOICES CASCADE CONSTRAINTS;
 
 -- ----------------------------------------------------
--- -- Part II: Create all tables ----------------------
+-- -- Part IIa: Create clusters -----------------------
+-- ----------------------------------------------------
+
+CREATE CLUSTER CLIENT_CLUSTER (CLIENTID VARCHAR2(15));
+CREATE CLUSTER MOVIE_CLUSTER (MOVIE_TITLE VARCHAR2(100));
+
+-- ----------------------------------------------------
+-- -- Part IIb: Create cluster index ------------------
+-- ----------------------------------------------------
+
+CREATE INDEX CLIENT_CLUSTER_INDEX ON CLUSTER CLIENT_CLUSTER;
+CREATE INDEX MOVIE_CLUSTER_INDEX ON CLUSTER MOVIE_CLUSTER;
+
+-- ----------------------------------------------------
+-- -- Part IIc: Create all tables ---------------------
 -- ----------------------------------------------------
 
 CREATE TABLE MOVIES(
 movie_title       VARCHAR2(100),
 title_year        NUMBER(4),
-country           VARCHAR2(25), 
+country           VARCHAR2(25),
 color             VARCHAR2(1),
 duration          NUMBER(3) NOT NULL,
-gross             NUMBER(10), 
+gross             NUMBER(10),
 budget            NUMBER(12),
 director_name     VARCHAR2(50) DEFAULT 'Anonymous',
 filming_language  VARCHAR2(20),
-num_critic_for_reviews    NUMBER(6), 
-director_facebook_likes   NUMBER(6), 
-num_voted_users           NUMBER(7), 
-num_user_for_reviews      NUMBER(6), 
-cast_total_facebook_likes NUMBER(6), 
-facenumber_in_poster      NUMBER(6), 
-movie_imdb_link           VARCHAR2(60), 
-imdb_score                NUMBER(2,1), 
+num_critic_for_reviews    NUMBER(6),
+director_facebook_likes   NUMBER(6),
+num_voted_users           NUMBER(7),
+num_user_for_reviews      NUMBER(6),
+cast_total_facebook_likes NUMBER(6),
+facenumber_in_poster      NUMBER(6),
+movie_imdb_link           VARCHAR2(60),
+imdb_score                NUMBER(2,1),
 content_rating            VARCHAR2(9),
-aspect_ratio              NUMBER(4,2) , 
+aspect_ratio              NUMBER(4,2) ,
 movie_facebook_likes      NUMBER(6),
 CONSTRAINT MOVIES_PK PRIMARY KEY (movie_title),
 CONSTRAINT MOVIES_CH CHECK (COLOR IN ('B','C', null))
 )
-CLUSTER movie_cluster(movie_title);
+CLUSTER MOVIE_CLUSTER(movie_title);
 
 
 CREATE TABLE GENRES_MOVIES (
@@ -81,13 +93,13 @@ CONSTRAINT FK_KEYWORDS_MOVIES FOREIGN KEY (title) REFERENCES MOVIES ON DELETE CA
 
 
 CREATE TABLE PLAYERS (
-actor_name      VARCHAR2(50), 
-facebook_likes  NUMBER(6,0), 
+actor_name      VARCHAR2(50),
+facebook_likes  NUMBER(6,0),
 CONSTRAINT ACTORS_PK PRIMARY KEY (actor_name));
 
 
 CREATE TABLE CASTS (
-actor   VARCHAR2(50), 
+actor   VARCHAR2(50),
 title   VARCHAR2(100),
 CONSTRAINT PK_CASTS PRIMARY KEY (actor, title),
 CONSTRAINT FK1_CASTS FOREIGN KEY (actor) REFERENCES PLAYERS ON DELETE CASCADE,
@@ -104,7 +116,7 @@ CONSTRAINT PK_SERIES PRIMARY KEY (title)
 
 CREATE TABLE SEASONS(
 title        	VARCHAR2(100),
-season 		NUMBER(3),	
+season 		NUMBER(3),
 avgduration	NUMBER(3) NOT NULL,
 episodes 	NUMBER(3) NOT NULL,
 CONSTRAINT PK_SEASONS PRIMARY KEY (title, season),
@@ -131,7 +143,7 @@ CLUSTER CLIENT_CLUSTER(CLIENTID);
 
 
 CREATE TABLE products(
-product_name 	VARCHAR2(25),  
+product_name 	VARCHAR2(25),
 fee		NUMBER(3) NOT NULL,
 type		VARCHAR2(1) NOT NULL,
 tap_cost	NUMBER(4,2) NOT NULL,
@@ -146,10 +158,10 @@ CONSTRAINT CK_products2 CHECK (PROMO <= 100)
 
 
 CREATE TABLE contracts(
-contractId VARCHAR2(10),  
-clientId  VARCHAR2(15),  
+contractId VARCHAR2(10),
+clientId  VARCHAR2(15),
 startdate DATE NOT NULL,
-enddate DATE, 
+enddate DATE,
 contract_type VARCHAR2(50),
 address		VARCHAR2(100) NOT NULL,
 town		VARCHAR2(100) NOT NULL,
@@ -164,35 +176,34 @@ CLUSTER CLIENT_CLUSTER(CLIENTID);
 
 
 CREATE TABLE taps_movies(
-contractId VARCHAR2(10), 
+contractId VARCHAR2(10),
 view_datetime DATE,
 pct	NUMBER(3) DEFAULT 0 NOT NULL,
-title VARCHAR2(100) NOT NULL, 
+title VARCHAR2(100) NOT NULL,
 CONSTRAINT PK_tapsM PRIMARY KEY (contractId,title,view_datetime),
 CONSTRAINT FK_tapsM1 FOREIGN KEY (contractId) REFERENCES contracts,
 CONSTRAINT FK_tapsM2 FOREIGN KEY (title) REFERENCES movies
 )
-CLUSTER CONTRACT_CLUSTER(CONTRACTID);
+CLUSTER MOVIE_CLUSTER(title);
 
 
 CREATE TABLE taps_series(
-contractId VARCHAR2(10), 
+contractId VARCHAR2(10),
 view_datetime DATE,
 pct	NUMBER(3) DEFAULT 0 NOT NULL,
-title VARCHAR2(100) NOT NULL, 
+title VARCHAR2(100) NOT NULL,
 season  NUMBER(3) NOT NULL,
 episode NUMBER(3) NOT NULL,
 CONSTRAINT PK_tapsS PRIMARY KEY (contractId,title,season,episode,view_datetime),
 CONSTRAINT FK_tapsS1 FOREIGN KEY (contractId) REFERENCES contracts,
 CONSTRAINT FK_tapsS2 FOREIGN KEY (title,season) REFERENCES seasons
-)
-CLUSTER CONTRACT_CLUSTER(CONTRACTID);
+);
 
 
 CREATE TABLE lic_movies(
-client VARCHAR2(15), 
+client VARCHAR2(15),
 datetime DATE,
-title VARCHAR2(100) NOT NULL, 
+title VARCHAR2(100) NOT NULL,
 CONSTRAINT PK_licsM PRIMARY KEY (client,title),
 CONSTRAINT FK_licsM1 FOREIGN KEY (title) REFERENCES movies,
 CONSTRAINT FK_licsM2 FOREIGN KEY (client) REFERENCES clients ON DELETE CASCADE
@@ -201,9 +212,9 @@ CLUSTER CLIENT_CLUSTER(CLIENT);
 
 
 CREATE TABLE lic_series(
-client VARCHAR2(15), 
+client VARCHAR2(15),
 datetime DATE,
-title VARCHAR2(100) NOT NULL, 
+title VARCHAR2(100) NOT NULL,
 season  NUMBER(3) NOT NULL,
 episode NUMBER(3) NOT NULL,
 CONSTRAINT PK_licsS PRIMARY KEY (client,title,season,episode),
@@ -214,7 +225,7 @@ CLUSTER CLIENT_CLUSTER(CLIENT);
 
 
 CREATE TABLE invoices(
-clientId VARCHAR2(15),  
+clientId VARCHAR2(15),
 month  VARCHAR2(2) ,
 year  VARCHAR2(4) ,
 amount NUMBER(8,2) NOT NULL,
@@ -222,6 +233,3 @@ CONSTRAINT PK_invcs PRIMARY KEY (clientId,month,year),
 CONSTRAINT FK_invcs FOREIGN KEY (clientId) REFERENCES clients
 )
 CLUSTER CLIENT_CLUSTER(CLIENTID);
-
-
-
